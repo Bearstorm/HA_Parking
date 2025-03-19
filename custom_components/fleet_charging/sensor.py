c35c9bd98b7be0931db22a35de49603690b6c01a
Bearstorm <bearstormchannel@gmail.com>
2025-03-19 22:27:51 +0100

    Update sensor.py

--- custom_components/fleet_charging/sensor.py
+++ custom_components/fleet_charging/sensor.py
@@ -4,14 +4,17 @@ from datetime import timedelta
 from .database import FleetDatabase
 
 async def async_setup_entry(hass, config_entry, async_add_entities):
+    """Inicializácia senzorov pri pridávaní integrácie."""
     db = hass.data["fleet_charging"]["db"]
     async_add_entities([
         ChargingSessionSensor(hass, db),
-        WallboxEnergySensor(hass, db),
-        ChargingStatisticsSensor(hass, db)
+        DailyReportSensor(hass, db),
+        WallboxStatusSensor(hass, db)
     ])
 
 class ChargingSessionSensor(SensorEntity):
+    """Senzor pre aktuálnu reláciu nabíjania."""
+
     def __init__(self, hass, db: FleetDatabase):
         self._hass = hass
         self._db = db
@@ -26,38 +29,20 @@ class ChargingSessionSensor(SensorEntity):
     async def async_update(self, now=None):
         session = await self._db.get_latest_session()
         if session:
-            vehicle = session.get('vehicle_id')
-            user = session.get('user_id')
-            wallbox = session.get('wallbox_id')
-            self._attr_state = f"Vozidlo: {vehicle}, Používateľ: {user}, Wallbox: {wallbox}"
+            vehicle = session.get('vehicle_name')
+            user = session.get('user_name')
+            self._attr_state = f"{vehicle} - {user}"
         else:
             self._attr_state = "Žiadna aktívna relácia"
 
-class WallboxEnergySensor(SensorEntity):
+class DailyReportSensor(SensorEntity):
+    """Senzor pre denný report nabíjania."""
+
     def __init__(self, hass, db: FleetDatabase):
         self._hass = hass
         self._db = db
-        self._attr_name = "Spotreba Wallboxu"
-        self._attr_state = "Neznáme"
-        async_track_time_interval(hass, self.async_update, timedelta(minutes=1))
-
-    @property
-    def state(self):
-        return self._attr_state
-
-    async def async_update(self, now=None):
-        energy_sensor = self._hass.states.get("sensor.wallbox_energy")
-        if energy_sensor:
-            self._attr_state = f"{energy_sensor.state} kWh"
-        else:
-            self._attr_state = "Dáta nie sú dostupné"
-
-class ChargingStatisticsSensor(SensorEntity):
-    def __init__(self, hass, db: FleetDatabase):
-        self._hass = hass
-        self._db = db
-        self._attr_name = "Štatistiky nabíjania"
-        self._attr_state = "Neznáme"
+        self._attr_name = "Denný report nabíjania"
+        self._attr_state = "Čaká sa na prvý report"
         async_track_time_interval(hass, self.async_update, timedelta(minutes=60))
 
     @property
@@ -65,10 +50,26 @@ class ChargingStatisticsSensor(SensorEntity):
         return self._attr_state
 
     async def async_update(self, now=None):
-        with sqlite3.connect(self._db.db_path) as conn:
-            cursor = conn.cursor()
-            cursor.execute("""
-                SELECT SUM(energy_consumed) FROM sessions WHERE timestamp >= datetime('now', '-30 days')
-            """)
-            total_energy = cursor.fetchone()[0]
-            self._attr_state = f"{total_energy:.2f} kWh" if total_energy else "0 kWh"
+        self._attr_state = await self._db.generate_daily_report()
+
+class WallboxStatusSensor(SensorEntity):
+    """Senzor pre stav Wallboxu a nabíjania."""
+
+    def __init__(self, hass, db: FleetDatabase):
+        self._hass = hass
+        self._db = db
+        self._attr_name = "Stav Wallboxu"
+        self._attr_state = "Neznámy"
+        async_track_time_interval(hass, self.async_update, timedelta(seconds=30))
+
+    @property
+    def state(self):
+        return self._attr_state
+
+    async def async_update(self, now=None):
+        wallbox_status = self._hass.states.get("sensor.05_cion_cp_signal_state")
+        if wallbox_status:
+            self._attr_state = wallbox_status.state
+        else:
+            self._attr_state = "Neznámy"
+
